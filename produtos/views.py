ee8b791f883bd2330942b138719397049131c92a
Paulo Victor <pvls2@ic.ufal.br>
2022-05-08 18:02:36 -0300

    feat: Views
    Cria as views que retorna os dados de produtos para montar a tabela.

--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import  render
+from .models import Produtos, DataConsulta
 
-# Create your views here.
+
+def formata_data(produto, datas):
+    dados = []
+    for data in datas:
+        try:
+            total_vendas = DataConsulta.objects.get(produto=produto, consult_date=data[0]).c
+        except:
+           total_vendas = 0
+        
+        dados.append({
+            'consult_date': data[0],
+            'c': total_vendas
+        })
+        
+    return dados
+    
+
+def ListaProdutos(request):
+    queryset = Produtos.objects.order_by('product_url')
+    datas = set(DataConsulta.objects.order_by('consult_date').values_list('consult_date'))
+    json_data = []
+    
+    for produto in queryset:
+        dados = {}
+                
+        consult_date = formata_data(produto, sorted(datas))
+        dados = {
+            'product_url': produto.product_url,
+            'product_url_created_at': produto.product_url_created_at,
+            'total': produto.total_vendas,
+            'consult_date': consult_date
+        }
+        
+        json_data.append(dados)
+        
+    return JsonResponse({'data': json_data})
--- projeto/urls.py
+++ projeto/urls.py
@@ -1,6 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+
+from produtos.views import ListaProdutos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('dados/', ListaProdutos, name='order_list_json'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
