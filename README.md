4a8068c85422f36e5471dffd2fd2e9ae222a5b49
Paulo Victor <pvls2@ic.ufal.br>
2022-05-08 22:14:10 -0300

    Deploy da aplicação

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# django-ajax
+1. Fork/Clone:
+
+1. Run:
+
+    ```bash
+    python manage.py migrate
+    ```
+    ```bash
+    python manage.py popula_db
+    ```
+    ```bash
+    python manage.py runserver
+    ```
+
+    Navigate to [http://localhost:8000/](http://localhost:8000/).
--- projeto/settings.py
+++ projeto/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -114,3 +115,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
